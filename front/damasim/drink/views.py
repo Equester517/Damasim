d6ae53cc6f33a14178194a617e6f87e8393d97c6
JoonHo <junohb135@gmail.com>
2021-11-14 15:26:36 +0900

    add Page transition

--- front/damasim/drink/views.py
+++ front/damasim/drink/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'drink/index.html')
+
+def recommend(request):
+    return render(request, 'drink/recommend.html')
