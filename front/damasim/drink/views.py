f7da0b71c9aa3dfde77e042c18826370c29d7805
DESKTOP-K3TC50U\qbsudet <quoobt@gmail.com>
2021-11-28 20:53:30 +0900

    coke and cokezero test

--- front/damasim/drink/views.py
+++ front/damasim/drink/views.py
@@ -10,28 +10,8 @@ def survey(request):
     return render(request, 'drink/survey.html')
 
 def dbtest(request):
-    #beverages = Bev14Prep.objects.all()
-
-    #coke = beverages.filter(myunknowncolumn=0)
 
     cokescore = Bev14Prep.objects.values("coca_cola_classic_coke")
+    cokezeroscore = Bev14Prep.objects.values("coca_cola_zero_sugar")
 
-    return render(request, 'drink/dbtest.html', {'cokescore': cokescore})
-
-    """
-    try:
-        cursor = connection.cursor()
-
-        strSql = "SELECT 코카콜라_coca_cola_classic_coke_field FROM beverage14.bev14_prep"
-        result = cursor.execute(strSql)
-        bevCoke = cursor.fetchall()
-
-        connection.commit()
-        connection.close()
-
-    except:
-        connection.rollback()
-        print("Failed selecting in beverage14.bev14_prep")
-
-    return render(request, 'dbtest.html', {'bevCoke': bevCoke})
-    """
+    return render(request, 'drink/dbtest.html', {'cokescore': cokescore, 'cokezeroscore':cokezeroscore})
