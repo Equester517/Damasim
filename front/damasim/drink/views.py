14950a78c17642375b3694bde6ce17ea05dd03ce
JoonHo <junohb3@naver.com>
2021-12-12 09:53:52 +0900

    Commit to merge

--- front/damasim/drink/views.py
+++ front/damasim/drink/views.py
@@ -25,9 +25,9 @@ def survey(request):
     for i in range(0, RAND_SIZE):
         # print(f'{columnname_list[random_index[i]]} 는 {random_index[i]}번째')
         columnname_list_shuffled.append(columnname_list[random_index[i]])
-        # columnname_list_shuffled.append(columnname_list[i])2
+        # columnname_list_shuffled.append(columnname_list[i])
     connection.close()
-    # print(columnname_list_shuffled)
+    print(columnname_list_shuffled)
 
     drink_List=zip(random_index, columnname_list_shuffled)
     # 랜덤하게 10개 받아서 인덱스 저장해놓고, 프론트로 그거로 html출력
@@ -55,35 +55,4 @@ def recommend(request):
             drink_List.append(columnname_list[index]) # 점수 상위 15개 음료수 이름목록
         print(drink_List)
         return render(request, 'drink/recommend.html', {'result': result, 'drink_List':drink_List})
-
-
-# def dbtest(request):
-
-#     curs = connection.cursor() #커서 연결
-#     sqlstr = "SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA`='rec' AND `TABLE_NAME`='db';" #Mysql Query
-#     curs.execute(sqlstr)
-#     columnnames_tp = curs.fetchall() #튜플 형태로 컬럼명을 받아옴
-#     columnname_list = [x[0] for x in columnnames_tp] #list comprehension
-#     # print(columnname_list)
-#     COL_SIZE = len(columnname_list) - 1 # Id컬럼을 제외한 아이템 수
-#     RAND_SIZE = 20 #랜덤하게 뽑을 숫자의 수
-
-#     random_index = random.sample(range(1,COL_SIZE + 1), RAND_SIZE) #랜덤하게 가져올 인덱스 숫자
-#     columnname_list_shuffled = []
-
-#     for i in range(1, RAND_SIZE):
-#         print(f'{columnname_list[random_index[i]]} 는 {random_index[i]}번째')
-#         columnname_list_shuffled.append(columnname_list[random_index[i]])
-#         # columnname_list_shuffled.append(columnname_list[i])
-#     connection.close()
-
-#     # print(columnname_list_shuffled)
-
-#     drink_List=zip(random_index, columnname_list_shuffled)
-#     # 랜덤하게 10개 받아서 인덱스 저장해놓고, 프론트로 그거로 html출력
-#     # 그 인덱스대로 점수 저장할 예정
-
-#     # return render(request, 'drink/dbtest.html', {'index':random_index, 'drink_List': columnname_list_shuffled})
-#     return render(request, 'drink/dbtest.html', {'List':drink_List})
-#     # return render(request, 'drink/dbtest.html')
 
